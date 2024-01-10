c064452e43e625ff2278f545e0986a66cb17d272
LWR <lwr1104@qq.com>
2024-01-10 20:03:29 +0800

    fix: Fixed ResponseCodeException -352 in dynamic push

--- starbot/utils/network.py
+++ starbot/utils/network.py
@@ -73,7 +73,7 @@ async def request(method: str,
     # 使用 Referer 和 UA 请求头以绕过反爬虫机制
     default_headers = {
         "Referer": "https://www.bilibili.com",
-        "User-Agent": "Mozilla/5.0"
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36 Core/1.94.218.400 QQBrowser/12.1.5496.400"
     }
     headers = default_headers
 
